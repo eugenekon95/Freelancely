311448cccb345da2ab9a4c660033800d17d6943f
Yevhenii Kononenko <evgenijkononenko0@gmail.com>
2021-06-11 21:47:54 +0300

    application helper update

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,8 +2,11 @@ module ApplicationHelper
   def avatar_url(user)
     if user.avatar.attached?
       url_for(user.avatar)
+    elsif user.image?
+      user.image
     else
-      ActionController::Base.helpers.asset_path('avatar.png')
+      ActionController::Base.helpers.asset_path('avatar.png')      
+
     end    
   end
 end
