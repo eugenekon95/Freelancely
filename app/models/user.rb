025769149deef5c74658f0a7d16f36131ee6dc76
Yevhenii Kononenko <evgenijkononenko0@gmail.com>
2021-06-13 10:56:44 +0300

    facebook verification added

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,14 +8,26 @@ class User < ApplicationRecord
   validates :full_name, presence: true, length: {minimum:6, maximum: 50}
 
   def self.from_omniauth(auth)
-    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-      user.email = auth.info.email
-      user.password = Devise.friendly_token[0, 20]
-      user.full_name = auth.info.name   # assuming the user model has a name
-      user.image = auth.info.image # assuming the user model has an image
-      # If you are using confirmable and the provider(s) you use validate emails, 
-      # uncomment the line below to skip the confirmation emails.
-      # user.skip_confirmation!
+    user = User.where(email: auth.info.email).first
+
+    if !user.provider
+      user.update(uid: auth.uid, provider: auth.provider, image: auth.info.image)
     end
+    return user
+    else
+      where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+        user.email = auth.info.email
+        user.password = Devise.friendly_token[0, 20]
+        user.full_name = auth.info.name   # assuming the user model has a name
+        user.image = auth.info.image # assuming the user model has an image
+        # If you are using confirmable and the provider(s) you use validate emails, 
+        # uncomment the line below to skip the confirmation emails.
+        # user.skip_confirmation!
+        user.uid = auth.uid
+        user.provider = auth.provider
+      end
+    end
+
+ 
   end
 end
